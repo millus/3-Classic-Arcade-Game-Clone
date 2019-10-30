29bc924499a723c44ea81229922948d647c154b6
Camilla Knutsen <camillaknutsen@Camillas-MacBook-Pro-2.local>
2019-10-30 15:46:42 +0100

    refactor: from ES5 to ES6 classes

--- js/app.js
+++ js/app.js
@@ -1,50 +1,59 @@
-// Enemies our player must avoid
-var Enemy = function(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Character {
+
+  constructor(x, y, sprite) {
     this.x = x;
     this.y = y;
-    this.speed = 5;
+    this.sprite = sprite;
+  }
 
+  render() { // Draw the enemy on the screen, required method for game
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
+
+// Enemies our player must avoid
+class Enemy extends Character {
+
+  constructor(x = 100, y = 150, speed = 3) {
+    super(x,y);
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
+  }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if(this.x>ctx.canvas.width){
+    if(this.x>ctx.canvas.width) {
       this.x = -50; //TODO: find out enemies width
       this.y = Math.floor(Math.random() * 200) + 10;
     }
     this.x = this.x + this.speed;
-
     this.x*dt;
-
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-function Player () {
-  this.x = 200;
-  this.y = 420;
-  this.sprite = 'images/char-pink-girl.png';
-  this.update = function(dt) {
+class Player extends Character {
+
+  constructor(x = 200, y = 420) {
+    super(x,y);
+    this.sprite = 'images/char-pink-girl.png';
   }
-  this.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+  update(dt) {
+
   }
-  this.handleInput = function(key) {
+
+  handleInput(key) {
     switch(key) {
       case 'up':
         if(this.y>0){
@@ -75,15 +84,15 @@ function Player () {
 // Place the player object in a variable called player
 let player = new Player();
 let enemy1 = new Enemy(100,150);
-let enemy2 = new Enemy(200,50);
-let enemy3 = new Enemy(20,220);
+let enemy2 = new Enemy(200,50,4);
+let enemy3 = new Enemy(20,220,4);
 let allEnemies = [enemy1, enemy2, enemy3];
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
